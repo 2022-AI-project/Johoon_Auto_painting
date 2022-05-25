275486b6c95c3dd91071df74193a813748a44218
johoon-Choi <johoon4687@gmail.com>
2022-05-25 22:23:27 +0900

    22.05.25

--- make_model.py
+++ make_model.py
@@ -6,20 +6,21 @@ from sklearn.model_selection import train_test_split
 class make_model():
     def __init__(self):
         self.categories = ["apple", "carrot", "orientalmelon", "strawberry", "tomato", "watermelon"]
+        self.nb_classes = len(self.categories)
+        self.image_w = 128   # width of image
+        self.image_h = 128   # height of image
+
         self.image_rotate()     # 전처리 완료 시 생략 
-        # self.make_npy_file()
-        # self.make_model()
+        self.make_npy_file()
+        self.make_model()
 
     # train data image 를 rotate 하는 method -> Data augmentation
     def image_rotate(self):
         caltech_dir = "./multi_img_data/imgs_others/train"          # train data directory
         saving_dir = "./multi_img_data/imgs_others/train_rotated"   # rotated train data directory
         # 6 categories -> 6 classes (6 labels)
-        
-        image_w = 128   # width of image
-        image_h = 128   # height of image
 
-        for idx, cat in enumerate(categories):          # 현재 6 개의 label 들에 대한 train data를 수집한다.
+        for idx, cat in enumerate(self.categories):          # 현재 6 개의 label 들에 대한 train data를 수집한다.
             image_dir = caltech_dir + "/" + cat         # 각 label 들의 train data image 는 caltech_dir에
             files = glob.glob(image_dir + "/*.png")     #   그 label 이름의 directory 내부에 *.png 파일로 저장되어 있다.
                                                         #   모든 *.png 파일을 files 에 저장한다.
@@ -27,36 +28,28 @@ class make_model():
             for i, f in enumerate(files):               #   모든 *.png 파일을 하나하나 작업을 진행한다.
                 img = Image.open(f)                     # img 로 현재 파일을 정의한 후
                 img = img.convert("RGB")                #   현재 image 를 RGB mode 로 변경한다.
-                img = img.resize((image_w, image_h))    # 현재 image 를 128 * 128 size 로 resizing 한다.
+                img = img.resize((self.image_w, self.image_h))    # 현재 image 를 128 * 128 size 로 resizing 한다.
                 white = (255, 255, 255)                 # 하얀색은 RGB로 [255, 255, 255] 이다.
                 for j in range(-9, 9):
                     img_name = f.split('.')
                     img_name = img_name[1].split("\\")
 
                     img_ro = img.rotate(20 * j, expand = 1, fillcolor = white)
-                    img_ro = img_ro.crop((img_ro.size[0]/2 - image_w/2, img_ro.size[1]/2 - image_h/2, img_ro.size[0]/2 + image_w/2, img_ro.size[1]/2 + image_h/2))
-                    save_dir = saving_dir + "/" + cat + "/" + img_name[1] + "_" + str(j + 18) + ".png"
-                    img_ro.save(save_dir)
+                    img_ro = img_ro.crop((img_ro.size[0]/2 - self.image_w/2, img_ro.size[1]/2 - self.image_h/2, img_ro.size[0]/2 + self.image_w/2, img_ro.size[1]/2 + self.image_h/2))
+                    finally_saving_dir = saving_dir + "/" + cat + "/" + img_name[1] + "_" + str(j + 18) + ".png"
+                    img_ro.save(finally_saving_dir)
 
     # *.npy file 을 만드는 method 이다.
     def make_npy_file(self):
-        # rotate 된 train data image 가 저장이 될 directory
+        # rotate 된 train data image 가 있는 directory
         caltech_dir_rotated = "./multi_img_data/imgs_others/train_rotated"
-
-        categories = ["apple", "carrot", "orientalmelon", "strawberry", "tomato", "watermelon"]
-        nb_classes = len(categories)
-
-        image_w = 128
-        image_h = 128
-
-        pixels = image_h * image_w * 3
 
         X = []
         y = []
 
-        for idx, cat in enumerate(categories):
+        for idx, cat in enumerate(self.categories):
             # one-hot 돌리기.
-            label = [0 for i in range(nb_classes)]
+            label = [0 for i in range(self.nb_classes)]
             label[idx] = 1
 
             # image_dir = caltech_dir + "/" + cat
@@ -65,28 +58,23 @@ class make_model():
             # files = glob.glob(image_dir + "/*.png")
             files_rotated = glob.glob(image_dir_rotated + "/*.png")
 
-            print(cat, " 회전된 사진 개수 : ", len(files_rotated))
+            print("회전된", cat, "사진 개수 : ", len(files_rotated))
         
             for i, f in enumerate(files_rotated):
                 img = Image.open(f)
                 img = img.convert("RGB")
-                img = img.resize((image_w, image_h))
+                img = img.resize((self.image_w, self.image_h))
                 data=np.asarray(img)
                
                 X.append(data)
                 y.append(label)
-                
-                if i % 700 == 0:
-                    print(cat, " : ", f)
 
         X = np.array(X)
         y = np.array(y)
-        # 1 0 0 0 이면 airplanes
-        # 0 1 0 0 이면 buddha 이런식
 
         X_train, X_test, y_train, y_test = train_test_split(X, y)
         xy = (X_train, X_test, y_train, y_test)
-        print(len(y))
+
         np.save("./numpy_data/multi_image_data.npy", xy)
 
     def make_model(self):
@@ -103,20 +91,12 @@ class make_model():
 
         X_train, X_test, y_train, y_test = np.load("./numpy_data/multi_image_data.npy", allow_pickle = True)
         
-        categories = ["apple", "carrot", "orientalmelon", "strawberry", "tomato", "watermelon"]
-        nb_classes = len(categories)
-        
         X_train = X_train.astype(float) / 255
         X_test = X_test.astype(float) / 255
 
         with K.tf_ops.device('/device:CPU:0'):
             model = Sequential()
             # first layer -> 입력에 대한 정보를 받는다.
-            print("X_train : ", len(X_train))
-            print("X_train[0] : ", len(X_train[0]))
-            print("X_train[0][0] : ", len(X_train[0][0]))
-            print("X_train[0][0][0] : ", len(X_train[0][0][0]))
-            print("shape :", X_train.shape[1:])
             model.add(Conv2D(32, (3, 3), padding="same", input_shape=X_train.shape[1:],
                              activation='relu'))
             model.add(MaxPooling2D(pool_size=(2, 2)))
@@ -129,7 +109,7 @@ class make_model():
             model.add(Flatten())
             model.add(Dense(256, activation='relu'))
             model.add(Dropout(0.5))
-            model.add(Dense(nb_classes, activation='softmax'))
+            model.add(Dense(self.nb_classes, activation='softmax'))
             model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
             model_dir = './model'
 
@@ -147,7 +127,6 @@ class make_model():
         # batch_size, epochs 조절해가면서 변화 확인
         print("정확도 : %.4f" % (model.evaluate(X_test, y_test)[1]))
 
-
         y_vloss = history.history['val_loss']
         y_loss = history.history['loss']
         
@@ -156,6 +135,7 @@ class make_model():
 
         x_len = np.arange(len(y_loss))
 
+        # Loss function 과 Accuracy function 을 출력한다.
         plt.figure(1)
         plt.plot(x_len, y_vloss, marker='.', c='red', label='val_set_loss')
         plt.plot(x_len, y_loss, marker='.', c='blue', label='train_set_loss')
